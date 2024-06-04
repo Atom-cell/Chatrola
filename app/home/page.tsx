85d6e30b2aeaef029d94976c962b4fc737ba2201
sani.usnain <sani.usnain@onescreensolutions.com>
2024-06-04 12:52:44 +0500

    Added basic chat send and receive

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,12 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, ChangeEvent } from 'react';
 import io, { Socket } from 'socket.io-client';
 
 export default function Home() {
-	const [sock, setSocket] = useState<Socket | null>();
+	const [socket, setSocket] = useState<Socket | null>();
 	const [id, setId] = useState('');
+	const [msgInput, setMsgInput] = useState("");
+	const [message, setMessage] = useState<string []>([])
 
 	useEffect(() => {
 		const newSocket = io('http://localhost:5000');
@@ -28,14 +30,45 @@ export default function Home() {
 		};
 	}, []);
 
+	useEffect(() => {
+        if (socket) {
+            const handleMessage = (socketMessage: string) => {
+                console.log("message array ", message, " socket ", socketMessage);
+                setMessage(prevMessage => [...prevMessage, socketMessage]);
+            };
+
+            socket.on('emitMessage', handleMessage);
+
+            return () => {
+                socket.off('emitMessage', handleMessage);
+            };
+        }
+    }, [socket, message]);
+
+	const sendMessage= () => {
+		socket?.emit('sendmessage', msgInput);
+		setMsgInput("");
+	}
+
+
+	const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            sendMessage();
+        }
+    };
+	
 	return (
 		<main>
+			<input type="text" value={msgInput} onKeyDown={handleKeyDown} onChange={(e: ChangeEvent<HTMLInputElement>) => setMsgInput(e.target.value)} className="text-black"/>
 			<button
 				className='bg-gray-500 p-2 m-2 hover:bg-gray-400'
-				onClick={() => sock?.emit('message', `Hello There ${id}`)}
+				onClick={() => sendMessage()}
 			>
-				Hello
+				Send
 			</button>
+			{message.map((data, index) => {
+				return <p key={index} className="text-xl text-white ">{data}</p>
+			})}
 		</main>
 	);
 }
