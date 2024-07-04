c289b2a3b77fea62c1a96230ceded74c44eaaaf0
sani.usnain <sani.usnain@onescreensolutions.com>
2024-07-04 13:47:06 +0500

    / removed from url

--- app/utils/ServerURI.ts
+++ app/utils/ServerURI.ts
@@ -1,5 +1,5 @@
 const serverURL: string =
 	process.env.NODE_ENV === 'production'
-		? 'https://chatrolaserver-production.up.railway.app/'
+		? 'https://chatrolaserver-production.up.railway.app'
 		: 'http://localhost:5000';
 export default serverURL;
