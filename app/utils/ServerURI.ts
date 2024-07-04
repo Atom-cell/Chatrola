bbca8944e47485586ea68023b1bd0a826a434bdd
sani.usnain <sani.usnain@onescreensolutions.com>
2024-07-04 13:42:28 +0500

    Server URL changed

--- app/utils/ServerURI.ts
+++ app/utils/ServerURI.ts
@@ -1,5 +1,5 @@
 const serverURL: string =
 	process.env.NODE_ENV === 'production'
-		? 'https://chatrola-server.vercel.app'
+		? 'https://chatrolaserver-production.up.railway.app/'
 		: 'http://localhost:5000';
 export default serverURL;
