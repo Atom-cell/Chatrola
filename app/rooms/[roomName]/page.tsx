6b47ab883c264a96d7e6c273eab17d0566574576
sani.usnain <sani.usnain@onescreensolutions.com>
2024-07-08 18:33:24 +0500

    Message div style fixed

--- app/rooms/[roomName]/page.tsx
+++ app/rooms/[roomName]/page.tsx
@@ -228,7 +228,7 @@ export default function Home({ params }: { params: { roomName: string } }) {
 	};
 
 	return (
-		<div className='w-full h-full flex flex-col shadow-md'>
+		<div className='w-full h-full flex flex-col shadow-md '>
 			<div className='flex items-center justify-between'>
 				<h2 className='md:text-3xl text-base'>Joyful Journey</h2>
 				{minutes && (
@@ -238,6 +238,7 @@ export default function Home({ params }: { params: { roomName: string } }) {
 						kickOutUsers={kickOutUsers}
 					/>
 				)}
+
 				<button
 					onClick={() => leaveRoom()}
 					className='m-4 bg-green-1 w-1/6 rounded py-2 px-1 md:text-xl text-md hover:bg-green-600'
@@ -245,49 +246,48 @@ export default function Home({ params }: { params: { roomName: string } }) {
 					Leave
 				</button>
 			</div>
+			{/* <div className='flex flex-col h-full'> */}
 
-			<div className='flex flex-col h-full'>
-				<div
-					className='flex-grow overflow-y-scroll mt-2 flex flex-col justify-end'
-					style={{ maxHeight: 'calc(100vh - 200px)' }}
+			<div
+				className='flex flex-col justify-end mt-2 flex-grow'
+				style={{ maxHeight: '60vh' }} // Adjust the 60px based on your header height
+			>
+				<div className='overflow-y-scroll flex flex-col'>
+					{message ? (
+						message.map((data, index) => (
+							<p
+								key={index}
+								className={`text-sm md:text-lg text-white tracking-tighter w-[90%] h-auto px-4 py-2 rounded ${
+									data.sender === name ? 'bg-green-1 self-end' : 'bg-gray-800'
+								} my-2`}
+							>
+								{data.msg}
+							</p>
+						))
+					) : (
+						<MessagesSkeleton />
+					)}
+					<div ref={endRef}></div>
+				</div>
+			</div>
+
+			<div className='flex items-center mb-2 mt-auto'>
+				<input
+					type='text'
+					placeholder='Type a message ...'
+					value={msgInput}
+					onKeyDown={handleKeyDown}
+					onChange={(e: ChangeEvent<HTMLInputElement>) =>
+						setMsgInput(e.target.value)
+					}
+					className='placeholder:italic text-black rounded py-1 px-2 text-lg focus:ring-2 focus:ring-green-1 focus:outline-none outline-none w-full tracking-tighter'
+				/>
+				<button
+					className='flex items-center justify-center py-2 px-4 rounded bg-green-1 m-2 hover:bg-green-600'
+					onClick={() => sendMessage()}
 				>
-					<div className='flex flex-col'>
-						{message ? (
-							message.map((data, index) => (
-								<p
-									key={index}
-									className={`text-sm md:text-lg text-white tracking-tighter w-[90%] h-auto px-4 py-2 rounded ${
-										data.sender === name ? 'bg-green-1 self-end' : 'bg-gray-800'
-									} my-2`}
-								>
-									{data.msg}
-								</p>
-							))
-						) : (
-							<MessagesSkeleton />
-						)}
-					</div>
-					{/* <div ref={endRef}></div> */}
-				</div>
-
-				<div className='flex items-center mb-2 mt-auto '>
-					<input
-						type='text'
-						placeholder='Type a message ...'
-						value={msgInput}
-						onKeyDown={handleKeyDown}
-						onChange={(e: ChangeEvent<HTMLInputElement>) =>
-							setMsgInput(e.target.value)
-						}
-						className='placeholder:italic text-black rounded py-1 px-2 text-lg focus:ring-2 focus:ring-green-1 focus:outline-none outline-none w-full tracking-tighter'
-					/>
-					<button
-						className='flex items-center justify-center py-2 px-4 rounded bg-green-1 m-2 hover:bg-green-600'
-						onClick={() => sendMessage()}
-					>
-						<SendIcon />
-					</button>
-				</div>
+					<SendIcon />
+				</button>
 			</div>
 			<Toaster />
 		</div>
