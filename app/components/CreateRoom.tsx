60c5fd68a16589220f200d78542d37e77f0dde6e
sani.usnain <sani.usnain@onescreensolutions.com>
2024-07-17 22:53:56 +0500

    Spinner on create room buttin

--- app/components/CreateRoom.tsx
+++ app/components/CreateRoom.tsx
@@ -129,7 +129,7 @@ const CreateRoom = () => {
 				</span>
 
 				{buttonText !== 'Join Room' ? (
-					<Button buttonText={buttonText} type='submit' loading={loading}/>
+					<Button buttonText={buttonText} type='submit' loading={true}/>
 				) : null}
 				<p className='text-red-500 h-3'>{error}</p>
 			</form>
